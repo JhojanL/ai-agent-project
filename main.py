5f28b1ec40131f4b4d0345c9f70da411cb9ea3e3
JhojanL <jhojan0917@gmail.com>
2026-07-13 16:56:43 -0500

    Add schema for get_files_info function and integrate into available functions; update system prompt for clarity on AI capabilities

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import argparse
+from email.mime import message
+import json
 import os
 
 from dotenv import load_dotenv
 from openai import OpenAI
 from openai.types.chat import ChatCompletion
 from prompts import system_prompt
+from call_function import available_functions
 
 def generate_content(client: OpenAI, messages: list[dict[str, str]]) -> ChatCompletion:
     return client.chat.completions.create(
         model="openrouter/free",
         messages=messages,
         temperature=0,
+        tools=available_functions
     )
 
 
@@ -44,7 +48,18 @@ def main() -> None:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage.prompt_tokens}")
         print(f"Response tokens: {response.usage.completion_tokens}")
-    print("Response:", response.choices[0].message.content)
+    
+    message = response.choices[0].message
+    if not message.tool_calls:
+        print("Response:")
+        print(message.content)
+        return
+
+    for tool_call in message.tool_calls:
+        if tool_call.type != "function":
+            continue
+        function_args = json.loads(tool_call.function.arguments or "{}")
+        print(f"Calling function: {tool_call.function.name}({function_args})")
 
 
 if __name__ == "__main__":
